2293061939e188b6f3aa4b0ff559150528b529d4
pcorpx <33910798+pcorpx@users.noreply.github.com>
2017-11-26 02:04:03 +0500

    Solution for task1

--- task_1/sumOfTwo.js
+++ task_1/sumOfTwo.js
@@ -1,3 +1,13 @@
 function sumOfTwo(array1, array2, x) {
-
+  isTrue = false;
+  array1.forEach(function(element, i) {
+    for (var j = 0; j < array2.length; j++) {
+      if (array2[j] + array1[i] === x) {
+        isTrue = true;
+      }
+    }
+  })
+  return isTrue;
 }
+
+
